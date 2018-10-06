97bd6ce2d4403e0d8774d9342283a39eccfbdd94
Ann Robson <nosbora@gmail.com>
2018-10-06 10:50:21 -0700

    develop rule request and return js. format analyzed URL with different color. change callout color with importance. run prettier. At end of run show "Done."

--- src/js/app.js
+++ src/js/app.js
@@ -5,33 +5,34 @@ var fet = {
   errorMsg: $('#errorMessage'),
   loading: $('.loading'),
   resultsTitle: $('#resultsTitle'),
-  analyzeFinal: $('#analyzeFinal'),
+  analysisSteps: $('#analysisSteps'),
   congratulations: $('.congratulations'),
 };
 
- $(document).ready(function() {
-  $( "#testFrontend" ).submit(function( event ) {
+$(document).ready(function () {
+  $("#testFrontend").submit(function (event) {
     event.preventDefault();
 
-    fet.resultsTitle.html('Suggestions for ' + $('#url').val());
+    fet.resultsTitle.html('Suggestions for <span class="urlToProcess">' + $('#url').val() + '</span>:');
     fet.container.html('');
     fet.errorMsg.html('');
+    fet.analysisSteps.html('');
     fet.loading.show();
-    
+
     $.ajax({
       type: "POST",
       url: "src/php/requestPage.php?url=" + $('#url').val(),
       data: $("#testFrontend").serialize(),
-      error: function(err) {
+      error: function (err) {
         fet.errorMsg.html('<span class="formSubmittedMsgAlert">Sorry. An error occurred.</span>');
         fet.loading.hide();
       },
-      success: function(data) {
+      success: function (data) {
         var res = JSON.parse(data);
-          // we only include 'status' if something goes wrong.
-        if(res && !res.status) {
+        // we only include 'status' if something goes wrong.
+        if (res && !res.status) {
           if (res.length > 0) {
-            res.forEach(function(sug) {
+            res.forEach(function (sug) {
               fet.populateAndAddSuggestion(sug);
             });
           }
@@ -39,16 +40,16 @@ var fet = {
           $.ajax({
             type: "POST",
             url: "src/php/analyzeHTMLHead.php?url=" + $('#url').val(),
-            error: function(err) {
+            error: function (err) {
               fet.errorMsg.html('<span class="formSubmittedMsgAlert">Sorry. An error occurred.</span>');
               fet.loading.hide();
             },
-            success: function(results) {
+            success: function (results) {
               // Splits off from how we did the outer tests, and is a mess.
               var res = JSON.parse(results);
-              if(res && !res.status) {
+              if (res && !res.status) {
                 if (res.length > 0) {
-                  res.forEach(function(sug) {
+                  res.forEach(function (sug) {
                     fet.populateAndAddSuggestion(sug);
                   });
                 }
@@ -59,16 +60,16 @@ var fet = {
           $.ajax({
             type: "POST",
             url: "src/php/analyzeHTMLBody.php?url=" + $('#url').val(),
-            error: function(err) { 
+            error: function (err) {
               fet.errorMsg.html('<span class="formSubmittedMsgAlert">Sorry. An error occurred.</span>');
               fet.loading.hide();
             },
-            success: function(results) {
+            success: function (results) {
               // Splits off from how we did the outer tests, and is a mess.
               var res = JSON.parse(results);
-              if(res && !res.status) {
+              if (res && !res.status) {
                 if (res.length > 0) {
-                  res.forEach(function(sug) {
+                  res.forEach(function (sug) {
                     fet.populateAndAddSuggestion(sug);
                   });
                 }
@@ -78,25 +79,29 @@ var fet = {
 
                 // finally, run rule tests that are slow.
                 // put working indicator and messsage at bottom.
-                fet.analyzeFinal.show();
+                fet.analysisSteps.show();
+                fet.analysisSteps.addClass('animatedEllipsis');
+                fet.analysisSteps.html('Running final tests');
                 $.ajax({
                   type: "POST",
                   url: "src/php/analyzeSlowFinal.php?url=" + $('#url').val(),
-                  error: function(err) { 
+                  error: function (err) {
                     fet.errorMsg.html('<span class="formSubmittedMsgAlert">Sorry. An error occurred.</span>');
                     fet.loading.hide();
+                    fet.analysisSteps.html('An error occurred.');
                   },
-                  success: function(results) {
+                  success: function (results) {
                     // Splits off from how we did the outer tests, and is a mess.
                     var res = JSON.parse(results);
-                    if(res && !res.status) {
+                    if (res && !res.status) {
                       if (res.length > 0) {
-                        res.forEach(function(sug) {
+                        res.forEach(function (sug) {
                           fet.populateAndAddSuggestion(sug);
                         });
                       }
                       // if no suggestions were found, give the a congratulations.
-                      fet.analyzeFinal.fadeOut();
+                      fet.analysisSteps.removeClass('animatedEllipsis');
+                      fet.analysisSteps.html('Done.');
                       if (fet.container.find('.suggestionTemplate').length === 0) {
                         fet.congratulations.show();
                       }
@@ -107,9 +112,11 @@ var fet = {
               }
             }
           });
-              
+
         } else {
-          if(res && res.curl_error) {
+          fet.analysisSteps.html('');
+          fet.resultsTitle.html('');
+          if (res && res.curl_error) {
             fet.errorMsg.html('<span class="formSubmittedMsgAlert">' + res.curl_error + '</span>');
           } else {
             fet.errorMsg.html('<span class="formSubmittedMsgAlert">Error requesting page.</span>');
@@ -120,11 +127,21 @@ var fet = {
     });
   });
 
- });
- 
-fet.populateAndAddSuggestion = function(sug) {
+});
+
+fet.populateAndAddSuggestion = function (sug) {
   // bad mix of jQuery and raw
   var suggestion = fet.suggestionTemplate.cloneNode(true);
+  //var levels = ['bd-callout-info', 'bd-callout-warning', 'bd-callout-error'];
+  var calloutClass = 'bd-callout-warning';
+
+  // we don't bother with a default or use info yet.
+  if (sug.weight >= 70) {
+    // whoa baby
+    calloutClass = 'bd-callout-danger';
+  }
+
+  suggestion.className = suggestion.className + ' ' + calloutClass;
   suggestion.querySelector('.suggestion-title').textContent = sug.title;
   suggestion.querySelector('.suggestion-description').innerHTML = sug.description;
   suggestion.removeAttribute('hidden');
